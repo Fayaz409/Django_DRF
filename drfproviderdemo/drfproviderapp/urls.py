904d93659af2dcc8352c1acc81fb37f8102e327e
Fayaz409 <ftunio06@gmail.com>
2025-04-30 11:48:09 +0500

    Implemented Viewsets for CRUD which make the api creation very easy but abstract

--- drfproviderdemo/drfproviderapp/urls.py
+++ drfproviderdemo/drfproviderapp/urls.py
@@ -1,14 +1,26 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('api/viewset-employees',EmployeeViewSet)
+
 urlpatterns = [
-   path('api/fbv-employees/',employee_list,name='employees-list'),
-   path('api/fbv-employees/<int:pk>',employee_detail,name='employee-detail'),
-   path('api/employee-list/',EmployeeList.as_view()),
-   path('api/employee-list/<int:pk>',EmployeeDetail.as_view()),
-   path('api/employees-list/',EmployeesList.as_view()),
-   path('api/employees-list/<int:pk>',EmployeesDetail.as_view()),
-   path('api/employees-lists/',EmployeeLists.as_view()),
-   path('api/employees-lists/<int:pk>',EmployeeDetails.as_view()),
+    path('',include(router.urls))
+]
+
+# urlpatterns = [
+#    path('api/fbv-employees/',employee_list,name='employees-list'),
+#    path('api/fbv-employees/<int:pk>',employee_detail,name='employee-detail'),
+#    path('api/employee-list/',EmployeeList.as_view()),
+#    path('api/employee-list/<int:pk>',EmployeeDetail.as_view()),
+#    path('api/employees-list/',EmployeesList.as_view()),
+#    path('api/employees-list/<int:pk>',EmployeesDetail.as_view()),
+#    path('api/employees-lists/',EmployeeLists.as_view()),
+#    path('api/employees-lists/<int:pk>',EmployeeDetails.as_view()),
+#    # path('/api/all-emplyees/<int:pk>',EmployeeViewSet.as_view()),
    
 
-]
+# ]
--- drfproviderdemo/drfproviderapp/views.py
+++ drfproviderdemo/drfproviderapp/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import mixins,generics
+from rest_framework import viewsets
 from .serializers import EmployeeSerializer
 # Create your views here.
 
@@ -113,3 +114,9 @@ class EmployeeLists(generics.ListCreateAPIView):
 class EmployeeDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    
